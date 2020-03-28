19f7b6b83922303829bb4b4c1c048577e168a96b
Georgi Vidinski <georgi.vidinski@collectivworks.com>
2020-03-28 08:18:51 +0200

    Second Spring Cleaning;

--- period_search_cuda_10.2_win/period_search/error.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include <cstdio>
-#include <cstdlib>
-
-void ErrorFunction(const char* buffer, int no_conversions) {
-    fprintf(stderr, "An error occurred. You entered:\n%s\n", buffer);
-    fprintf(stderr, "%d successful conversions", no_conversions);
-    exit(EXIT_FAILURE);
-}
--- period_search_cuda_win/period_search/start_CUDA.h
+++
@@ -1,9 +0,0 @@
-int CUDAPrepare(int cudadev,double *beta_pole,double *lambda_pole,double *par,double cl,
-	            double Alamda_start,double Alamda_incr,double ee[][3],double ee0[][3],double *tim,
-				double Phi_0,int checkex,int ndata);
-int CUDAStart(int n_start_from,double freq_start,double freq_end,double freq_step,double stop_condition,int n_iter_min,double conw_r,
-	          int ndata,int *ia,int *ia_par,double *cg_first,MFILE& mf,double escl,double *sig,int Numfac,double *brightness);
-int CUDAPrecalc(double freq_start,double freq_end,double freq_step,double stop_condition,int n_iter_min,double *conw_r,
-	          int ndata,int *ia,int *ia_par,int *new_conw,double *cg_first,double *sig,int Numfac,double *brightness);
-int do_checkpoint(MFILE& mf, int nlines, int new_conw, double conwr);
-void CUDAUnprepare(void);
