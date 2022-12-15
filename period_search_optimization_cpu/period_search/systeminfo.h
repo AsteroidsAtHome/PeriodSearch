7df2cbeb1784e0a7515569e00ba45d938f5ecbf7
Georgi Vidinski <gvidinski@gmail.com>
2022-12-15 07:22:51 +0200

    RPi: New Makefiles for armv7 & armv8 platforms; Adds System information about the PRi boards to the stderr.txt;

---
+++ period_search_optimization_cpu/period_search/systeminfo.h
@@ -0,0 +1,8 @@
+#pragma once
+//#include <iostream>
+//#include <sstream>
+//#include <string>
+
+void getSystemInfo();
+void getCpuInfoByArch(std::ifstream &cpuinfo);
+float getTotalSystemMemory();
