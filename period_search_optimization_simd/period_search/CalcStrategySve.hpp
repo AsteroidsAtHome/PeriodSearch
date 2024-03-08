b9e029b2e82ad692d7f1345e0eabd82fcaa6be53
Pavel Rosický <pavel.rosicky@easy.cz>
2024-03-08 16:12:05 +0100

    fix sve

--- period_search_optimization_simd/period_search/CalcStrategySve.hpp
+++ period_search_optimization_simd/period_search/CalcStrategySve.hpp
@@ -30,10 +30,37 @@ public:
 	virtual void gauss_errc(double** a, int n, double b[], int &error);
 
 private:
-	int incl[MAX_N_FAC]; //array of indexes of facets to Area, Dg, Nor. !!!!!!!!!!!incl IS ZERO INDEXED
+    //svfloat64_t* Dg_row[MAX_N_FAC + 3];
+	//svfloat64_t dbr[MAX_N_FAC + 3];
+	//TODO
 
-	int incl_count;
+	double php[N_PHOT_PAR + 1];
+	double dphp[N_PHOT_PAR + 1];
+	double dbr[MAX_N_FAC]; //IS ZERO INDEXED
+	double e[4];
+	double e0[4];
+	double de[4][4];
+	double de0[4][4];
+	double tmat[4][4];
+	double dtm[4][4][4];
+
+	double cos_alpha;
+	double alpha;
+	double cl;
+	double cls;
+	double dnom;
+	double tmpdyda;
+	double s;
+
+	double tmpdyda1;
+	double tmpdyda2;
+	double tmpdyda3;
+	double tmpdyda4;
+	double tmpdyda5;
+
+	int	incl[MAX_N_FAC]; //array of indexes of facets to Area, Dg, Nor. !!!!!!!!!!!incl IS ZERO INDEXED
 	int ncoef0;
+	int incl_count = 0;
 };
 
 #endif
