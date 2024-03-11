11150b998b27b0769e9ae027ab0a378e99b422a6
Pavel Rosický <pavel.rosicky@easy.cz>
2024-03-11 11:00:39 +0100

    sve update

--- period_search_optimization_simd/period_search/CalcStrategySve.hpp
+++ period_search_optimization_simd/period_search/CalcStrategySve.hpp
@@ -30,9 +30,26 @@ public:
 	virtual void gauss_errc(double** a, int n, double b[], int &error);
 
 private:
-    //svfloat64_t* Dg_row[MAX_N_FAC + 3];
-	//svfloat64_t dbr[MAX_N_FAC + 3];
-	//TODO
+    /*
+	svfloat64_t* Dg_row[MAX_N_FAC + 3]{};
+	svfloat64_t dbr[MAX_N_FAC + 3]{};
+
+	double alpha = 0.0;
+	double cos_alpha = 0.0;
+	double cl = 0.0;
+	double cls = 0.0;
+	double e[4]{};
+	double e0[4]{};
+	double php[N_PHOT_PAR + 1]{};
+	double dphp[N_PHOT_PAR + 1]{};
+	double de[4][4]{};
+	double de0[4][4]{};
+	double tmat[4][4]{};
+	double dtm[4][4][4]{};
+
+	int	ncoef0 = 0;
+	int incl_count = 0;
+	*/
 
 	double php[N_PHOT_PAR + 1];
 	double dphp[N_PHOT_PAR + 1];
--- period_search_optimization_simd/period_search/bright_sve_draft.cpp
+++ period_search_optimization_simd/period_search/bright_sve_draft.cpp
@@ -14,44 +14,44 @@
 
 #define INNER_CALC \
     res_br = svadd_f64_x(pg, res_br, avx_pbr); \
-    svfloat64_t sve_sum1, sve_sum10, sve_sum2, sve_sum20, sve_sum3, sve_sum30; \
+    svfloat64_t avx_sum1, avx_sum10, avx_sum2, avx_sum20, avx_sum3, avx_sum30; \
     \
-    sve_sum1 = svmul_f64_x(pg, avx_Nor1, avx_de11); \
-    sve_sum1 = svmla_f64_x(pg, sve_sum1, avx_Nor2, avx_de21); \
-    sve_sum1 = svmla_f64_x(pg, sve_sum1, avx_Nor3, avx_de31); \
+    avx_sum1 = svmul_f64_x(pg, avx_Nor1, avx_de11); \
+    avx_sum1 = svmla_f64_x(pg, avx_sum1, avx_Nor2, avx_de21); \
+    avx_sum1 = svmla_f64_x(pg, avx_sum1, avx_Nor3, avx_de31); \
     \
-    sve_sum10 = svmul_f64_x(pg, avx_Nor1, avx_de011); \
-    sve_sum10 = svmla_f64_x(pg, sve_sum10, avx_Nor2, avx_de021); \
-    sve_sum10 = svmla_f64_x(pg, sve_sum10, avx_Nor3, avx_de031); \
+    avx_sum10 = svmul_f64_x(pg, avx_Nor1, avx_de011); \
+    avx_sum10 = svmla_f64_x(pg, avx_sum10, avx_Nor2, avx_de021); \
+    avx_sum10 = svmla_f64_x(pg, avx_sum10, avx_Nor3, avx_de031); \
     \
-    sve_sum2 = svmul_f64_x(pg, avx_Nor1, avx_de12); \
-    sve_sum2 = svmla_f64_x(pg, sve_sum2, avx_Nor2, avx_de22); \
-    sve_sum2 = svmla_f64_x(pg, sve_sum2, avx_Nor3, avx_de32); \
+    avx_sum2 = svmul_f64_x(pg, avx_Nor1, avx_de12); \
+    avx_sum2 = svmla_f64_x(pg, avx_sum2, avx_Nor2, avx_de22); \
+    avx_sum2 = svmla_f64_x(pg, avx_sum2, avx_Nor3, avx_de32); \
     \
-    sve_sum20 = svmul_f64_x(pg, avx_Nor1, avx_de012); \
-    sve_sum20 = svmla_f64_x(pg, sve_sum20, avx_Nor2, avx_de022); \
-    sve_sum20 = svmla_f64_x(pg, sve_sum20, avx_Nor3, avx_de032); \
+    avx_sum20 = svmul_f64_x(pg, avx_Nor1, avx_de012); \
+    avx_sum20 = svmla_f64_x(pg, avx_sum20, avx_Nor2, avx_de022); \
+    avx_sum20 = svmla_f64_x(pg, avx_sum20, avx_Nor3, avx_de032); \
     \
-    sve_sum3 = svmul_f64_x(pg, avx_Nor1, avx_de13); \
-    sve_sum3 = svmla_f64_x(pg, sve_sum3, avx_Nor2, avx_de23); \
-    sve_sum3 = svmla_f64_x(pg, sve_sum3, avx_Nor3, avx_de33); \
+    avx_sum3 = svmul_f64_x(pg, avx_Nor1, avx_de13); \
+    avx_sum3 = svmla_f64_x(pg, avx_sum3, avx_Nor2, avx_de23); \
+    avx_sum3 = svmla_f64_x(pg, avx_sum3, avx_Nor3, avx_de33); \
     \
-    sve_sum30 = svmul_f64_x(pg, avx_Nor1, avx_de013); \
-    sve_sum30 = svmla_f64_x(pg, sve_sum30, avx_Nor2, avx_de023); \
-    sve_sum30 = svmla_f64_x(pg, sve_sum30, avx_Nor3, avx_de033); \
+    avx_sum30 = svmul_f64_x(pg, avx_Nor1, avx_de013); \
+    avx_sum30 = svmla_f64_x(pg, avx_sum30, avx_Nor2, avx_de023); \
+    avx_sum30 = svmla_f64_x(pg, avx_sum30, avx_Nor3, avx_de033); \
     \
-    sve_sum1 = svmul_f64_x(pg, sve_sum1, avx_dsmu); \
-    sve_sum2 = svmul_f64_x(pg, sve_sum2, avx_dsmu); \
-    sve_sum3 = svmul_f64_x(pg, sve_sum3, avx_dsmu); \
-    sve_sum10 = svmul_f64_x(pg, sve_sum10, avx_dsmu0); \
-    sve_sum20 = svmul_f64_x(pg, sve_sum20, avx_dsmu0); \
-    sve_sum30 = svmul_f64_x(pg, sve_sum30, avx_dsmu0); \
+    avx_sum1 = svmul_f64_x(pg, avx_sum1, avx_dsmu); \
+    avx_sum2 = svmul_f64_x(pg, avx_sum2, avx_dsmu); \
+    avx_sum3 = svmul_f64_x(pg, avx_sum3, avx_dsmu); \
+    avx_sum10 = svmul_f64_x(pg, avx_sum10, avx_dsmu0); \
+    avx_sum20 = svmul_f64_x(pg, avx_sum20, avx_dsmu0); \
+    avx_sum30 = svmul_f64_x(pg, avx_sum30, avx_dsmu0); \
     \
-    avx_dyda1 = svmla_f64_x(pg, avx_Area, svadd_f64_x(pg, sve_sum1, sve_sum10), avx_dyda1); \
-    avx_dyda2 = svmla_f64_x(pg, avx_Area, svadd_f64_x(pg, sve_sum2, sve_sum20), avx_dyda2); \
-    avx_dyda3 = svmla_f64_x(pg, avx_Area, svadd_f64_x(pg, sve_sum3, sve_sum30), avx_dyda3); \
+    avx_dyda1 = svmla_f64_x(pg, avx_dyda1, avx_Area, svadd_f64_x(pg, avx_sum1, avx_sum10)); \
+    avx_dyda2 = svmla_f64_x(pg, avx_dyda2, avx_Area, svadd_f64_x(pg, avx_sum2, avx_sum20)); \
+    avx_dyda3 = svmla_f64_x(pg, avx_dyda3, avx_Area, svadd_f64_x(pg, avx_sum3, avx_sum30)); \
     \
-    avx_d = svmla_f64_x(pg, avx_Area, svmul_f64_x(pg, avx_lmu, avx_lmu0), avx_d); \
+    avx_d = svmla_f64_x(pg, avx_d, avx_Area, svmul_f64_x(pg, avx_lmu, avx_lmu0)); \
     avx_d1 = svadd_f64_x(pg, avx_d1, svdiv_f64_x(pg, svmul_f64_x(pg, svmul_f64_x(pg, avx_Area, avx_lmu), avx_lmu0), svadd_f64_x(pg, avx_lmu, avx_lmu0)));
 
 // end of inner_calc
@@ -64,10 +64,10 @@
     svfloat64_t avx_pbr = svmul_f64_x(pg, avx_Area, avx_s); \
     svfloat64_t avx_powdnom = svdiv_f64_x(pg, avx_lmu0, avx_dnom); \
     avx_powdnom = svmul_f64_x(pg, avx_powdnom, avx_powdnom); \
-    svfloat64_t avx_dsmu = svmla_f64_x(pg, avx_cl, avx_lmu0, svmul_f64_x(pg, avx_cls, avx_powdnom)); \
+    svfloat64_t avx_dsmu = svmla_f64_x(pg, svmul_f64_x(pg, avx_cls, avx_powdnom), avx_cl, avx_lmu0); \
     avx_powdnom = svdiv_f64_x(pg, avx_lmu, avx_dnom); \
     avx_powdnom = svmul_f64_x(pg, avx_powdnom, avx_powdnom); \
-    svfloat64_t avx_dsmu0 = svmla_f64_x(pg, avx_cl, avx_lmu, svmul_f64_x(pg, avx_cls, avx_powdnom));
+    svfloat64_t avx_dsmu0 = svmla_f64_x(pg, svmul_f64_x(pg, avx_cls, avx_powdnom), avx_cl, avx_lmu);
 // end of inner_calc_dsmu
 
 
@@ -77,7 +77,7 @@ __attribute__((__target__("+sve")))
 #endif
 void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], double dyda[], int ncoef, double &br)
 {
-	int i, j, k; //ncoef0,
+	int i, j, k;
 	incl_count = 0;
 
 	//double cos_alpha, cl, cls, alpha, //br
@@ -93,6 +93,7 @@ void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], d
     cls = cg[ncoef];					/* Lommel-Seeliger */
     //cos_alpha = dot_product(ee, ee0);
     dot_product_new(ee, ee0, cos_alpha);
+	alpha = acos(cos_alpha);
 	for (i = 1; i <= Nphpar; i++)
 		php[i] = cg[ncoef0 + i];
 
@@ -100,9 +101,8 @@ void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], d
 
 	matrix(cg[ncoef0], t, tmat, dtm);
 
-	//   br = 0;
-	   /* Directions (and ders.) in the rotating system */
-
+	// br = 0;
+    /* Directions (and ders.) in the rotating system */
 	for (i = 1; i <= 3; i++)
 	{
 		e[i] = 0;
@@ -122,38 +122,35 @@ void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], d
 	}
 
 	/*Integrated brightness (phase coeff. used later) */
-	svfloat64_t avx_e1 = svdup_f64(e[1]);
-	svfloat64_t avx_e2 = svdup_f64(e[2]);
-	svfloat64_t avx_e3 = svdup_f64(e[3]);
-	svfloat64_t avx_e01 = svdup_f64(e0[1]);
-	svfloat64_t avx_e02 = svdup_f64(e0[2]);
-	svfloat64_t avx_e03 = svdup_f64(e0[3]);
-	svfloat64_t avx_de11 = svdup_f64(de[1][1]);
-	svfloat64_t avx_de12 = svdup_f64(de[1][2]);
-	svfloat64_t avx_de13 = svdup_f64(de[1][3]);
-	svfloat64_t avx_de21 = svdup_f64(de[2][1]);
-	svfloat64_t avx_de22 = svdup_f64(de[2][2]);
-	svfloat64_t avx_de23 = svdup_f64(de[2][3]);
-	svfloat64_t avx_de31 = svdup_f64(de[3][1]);
-	svfloat64_t avx_de32 = svdup_f64(de[3][2]);
-	svfloat64_t avx_de33 = svdup_f64(de[3][3]);
-	svfloat64_t avx_de011 = svdup_f64(de0[1][1]);
-	svfloat64_t avx_de012 = svdup_f64(de0[1][2]);
-	svfloat64_t avx_de013 = svdup_f64(de0[1][3]);
-	svfloat64_t avx_de021 = svdup_f64(de0[2][1]);
-	svfloat64_t avx_de022 = svdup_f64(de0[2][2]);
-	svfloat64_t avx_de023 = svdup_f64(de0[2][3]);
-	svfloat64_t avx_de031 = svdup_f64(de0[3][1]);
-	svfloat64_t avx_de032 = svdup_f64(de0[3][2]);
-	svfloat64_t avx_de033 = svdup_f64(de0[3][3]);
+	svfloat64_t avx_e1 = svdup_n_f64(e[1]);
+	svfloat64_t avx_e2 = svdup_n_f64(e[2]);
+	svfloat64_t avx_e3 = svdup_n_f64(e[3]);
+	svfloat64_t avx_e01 = svdup_n_f64(e0[1]);
+	svfloat64_t avx_e02 = svdup_n_f64(e0[2]);
+	svfloat64_t avx_e03 = svdup_n_f64(e0[3]);
+	svfloat64_t avx_de11 = svdup_n_f64(de[1][1]);
+	svfloat64_t avx_de12 = svdup_n_f64(de[1][2]);
+	svfloat64_t avx_de13 = svdup_n_f64(de[1][3]);
+	svfloat64_t avx_de21 = svdup_n_f64(de[2][1]);
+	svfloat64_t avx_de22 = svdup_n_f64(de[2][2]);
+	svfloat64_t avx_de23 = svdup_n_f64(de[2][3]);
+	svfloat64_t avx_de31 = svdup_n_f64(de[3][1]);
+	svfloat64_t avx_de32 = svdup_n_f64(de[3][2]);
+	svfloat64_t avx_de33 = svdup_n_f64(de[3][3]);
+	svfloat64_t avx_de011 = svdup_n_f64(de0[1][1]);
+	svfloat64_t avx_de012 = svdup_n_f64(de0[1][2]);
+	svfloat64_t avx_de013 = svdup_n_f64(de0[1][3]);
+	svfloat64_t avx_de021 = svdup_n_f64(de0[2][1]);
+	svfloat64_t avx_de022 = svdup_n_f64(de0[2][2]);
+	svfloat64_t avx_de023 = svdup_n_f64(de0[2][3]);
+	svfloat64_t avx_de031 = svdup_n_f64(de0[3][1]);
+	svfloat64_t avx_de032 = svdup_n_f64(de0[3][2]);
+	svfloat64_t avx_de033 = svdup_n_f64(de0[3][3]);
 	svfloat64_t avx_tiny = svdup_n_f64(TINY);
-
 	svfloat64_t avx_cl = svdup_n_f64(cl);
-	svfloat64_t avx_cl1 = svset_f64(svptrue_b64(), 2, cl);
 	svfloat64_t avx_cls = svdup_n_f64(cls);
 	svfloat64_t avx_11 = svdup_n_f64(1.0);
-
-	svfloat64_t avx_Scale = svdup_f64(Scale);
+	svfloat64_t avx_Scale = svdup_n_f64(Scale);
 	svfloat64_t res_br = svdup_n_f64(0.0);
 	svfloat64_t avx_dyda1 = svdup_n_f64(0.0);
 	svfloat64_t avx_dyda2 = svdup_n_f64(0.0);
@@ -161,13 +158,13 @@ void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], d
 	svfloat64_t avx_d = svdup_n_f64(0.0);
 	svfloat64_t avx_d1 = svdup_n_f64(0.0);
 	int cnt = svcntd();
-	double g[cnt];
+	double g[svcntd()];
 
 	for (i = 0; i < Numfac; i += cnt)
 	{
 		svbool_t pg = svwhilelt_b64(i, Numfac);
 
-		svfloat64_t avx_lmu, avx_lmu0, cmpe, cmpe0, cmp;
+		svfloat64_t avx_lmu, avx_lmu0;
 		svfloat64_t avx_Nor1 = svld1_f64(pg, &Nor[0][i]);
 		svfloat64_t avx_Nor2 = svld1_f64(pg, &Nor[1][i]);
 		svfloat64_t avx_Nor3 = svld1_f64(pg, &Nor[2][i]);
@@ -175,24 +172,46 @@ void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], d
 		svfloat64_t avx_Area;
 
 		avx_lmu = svmul_f64_x(pg, avx_e1, avx_Nor1);
-		avx_lmu = svmla_f64_x(pg, avx_e2, avx_Nor2, avx_lmu);
-		avx_lmu = svmla_f64_x(pg, avx_e3, avx_Nor3, avx_lmu);
+		avx_lmu = svmla_f64_x(pg, avx_lmu, avx_e2, avx_Nor2);
+		avx_lmu = svmla_f64_x(pg, avx_lmu, avx_e3, avx_Nor3);
 
 		avx_lmu0 = svmul_f64_x(pg, avx_e01, avx_Nor1);
 		avx_lmu0 = svmla_f64_x(pg, avx_lmu0, avx_e02, avx_Nor2);
 		avx_lmu0 = svmla_f64_x(pg, avx_lmu0, avx_e03, avx_Nor3);
 
-		cmpe = svcgt_f64(pg, avx_lmu, avx_tiny);
-		cmpe0 = svcgt_f64(pg, avx_lmu0, avx_tiny);
-		cmp = svand_f64(pg, cmpe, cmpe0);
+		svbool_t cmpe = svcmpgt_f64(pg, avx_lmu, avx_tiny);
+		svbool_t cmpe0 = svcmpgt_f64(pg, avx_lmu0, avx_tiny);
+		svbool_t cmp = svand_z(pg, cmpe, cmpe0);
 
-		int icmp = svcvt_z_p_b64(cmp);
+		/* find a better solution
+		svbool_t pred = svptrue_b64();
+		int icmp = 0;
+    	for (int i = 0; i < cnt; i++) {
+        	if (svptest_first(pred) {
+			  icmp |= 1 << b;
+			}
+        	pred = svptest_next(pred);
+    	}
+		*/
 
-		if (icmp)
+        // workaround
+		double x_cmpe[svcntd()];
+		double x_cmpe0[svcntd()];
+		svst1_f64(pg, x_cmpe, avx_lmu);
+		svst1_f64(pg, x_cmpe0, avx_lmu0);
+		int icmp = 0;
+		for (int b = 0; b < svcntd(); b++) {
+		  	if (x_cmpe[b] > TINY && x_cmpe0[b] > TINY) {
+                icmp |= 1 << b;
+			}
+		}
+		// TODO workaround
+
+		if (svptest_any(pg, cmp)) //if (icmp)
 		{
 			INNER_CALC_DSMU
 
-			svfloat64_t avx_zero = svdup_f64(0.0);
+			svfloat64_t avx_zero = svdup_n_f64(0.0);
 			avx_pbr = svsel_f64(cmp, avx_pbr, avx_zero);
 			avx_dsmu = svsel_f64(cmp, avx_dsmu, avx_zero);
 			avx_dsmu0 = svsel_f64(cmp, avx_dsmu0, avx_zero);
@@ -200,73 +219,23 @@ void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], d
 			avx_lmu0 = svsel_f64(cmp, avx_lmu0, avx_11); //abychom nedelili nulou
 
 			svst1_f64(pg, g, avx_pdbr);
-			if (icmp & 1)
-			{
-				Dg_row[incl_count] = (svfloat64_t*)&Dg[i];
-				dbr[incl_count++] = svdup_f64(g[0]);
-			}
-			if (icmp & 2)
-			{
-				Dg_row[incl_count] = (svfloat64_t*)&Dg[i + 1];
-				dbr[incl_count++] = svdup_f64(g[1]);
-			}
-			if (icmp & 4)
-			{
-				Dg_row[incl_count] = (svfloat64_t*)&Dg[i + 2];
-				dbr[incl_count++] = svdup_f64(g[2]);
-			}
-			if (icmp & 8)
-			{
-				Dg_row[incl_count] = (svfloat64_t*)&Dg[i + 3];
-				dbr[incl_count++] = svdup_f64(g[3]);
+			for (int j = 0; j < cnt; j++) {
+    			if (icmp & (1 << j)) {
+        			Dg_row[incl_count] = (svfloat64_t*)&Dg[i + j];
+        			dbr[incl_count++] = svdup_n_f64(g[j]);
+    			}
 			}
 			INNER_CALC
 		}
 	}
 
 	dbr[incl_count] = svdup_n_f64(0.0);
-	//   dbr[incl_count+1]=svdup_n_f64(0.0);
 	Dg_row[incl_count] = Dg_row[0];
-	//   Dg_row[incl_count+1] = Dg_row[0];
-
-	res_br = svadd_f64(pg, svget2_f64(res_br, 0), svget2_f64(res_br, 1));
-	res_br = svadd_f64(pg, res_br, svext_f64(res_br, svres_f64(), 1));
-	svst1_f64(pg, g, res_br);
-	br = g[0];
+    br = svaddv_f64(svptrue_b64(), res_br);
 
 	/* Derivatives of brightness w.r.t. g-coeffs */
-	int ncoef03 = ncoef0 - 3, dgi = 0, cyklus1 = (ncoef03 / 12) * 12;
-
-	for (i = 0; i < cyklus1; i += 12) //3 * 4doubles
-	{
-		svfloat64_t tmp1 = svdup_n_f64(0.0);
-		svfloat64_t tmp2 = svdup_n_f64(0.0);
-		svfloat64_t tmp3 = svdup_n_f64(0.0);
-
-		for (j = 0; j < incl_count; j += 2)
-		{
-			svbool_t pg2 = svwhilelt_b64(j, incl_count);
-
-			svfloat64_t *Dgrow, *Dgrow1, pdbr, pdbr1;
-
-			Dgrow = &Dg_row[j][dgi];
-			pdbr = dbr[j];
-			Dgrow1 = &Dg_row[j + 1][dgi];
-			pdbr1 = dbr[j + 1];
-
-			tmp1 = svmla_f64_x(pg2, svmla_f64_x(pg2, tmp1, pdbr, Dgrow[0]), pdbr1, Dgrow1[0]);
-			tmp2 = svmla_f64_x(pg2, svmla_f64_x(pg2, tmp2, pdbr, Dgrow[1]), pdbr1, Dgrow1[1]);
-			tmp3 = svmla_f64_x(pg2, svmla_f64_x(pg2, tmp3, pdbr, Dgrow[2]), pdbr1, Dgrow1[2]);
-		}
-		dgi += 3;
-		tmp1 = svmul_f64_x(pg, tmp1, avx_Scale)
-		svst1_f64(pg, &dyda[i], tmp1);
-		tmp2 = svmul_f64_x(pg, tmp2, avx_Scale)
-		svst1_f64(pg, &dyda[i + 4], tmp2);
-		tmp3 = svmul_f64_x(pg, tmp3, avx_Scale)
-		svst1_f64(pg, &dyda[i + 8], tmp3);
-	}
-	for (; i < ncoef03; i += 4) //1 * 4doubles
+	int ncoef03 = ncoef0 - 3, dgi = 0;
+	for (i = 0; i < ncoef03; i += cnt) //1 * cnt doubles
 	{
 		svfloat64_t tmp1 = svdup_n_f64(0.0);
 
@@ -274,58 +243,30 @@ void CalcStrategySve::bright(double ee[], double ee0[], double t, double cg[], d
 		{
 			svfloat64_t *Dgrow, *Dgrow1, pdbr, pdbr1;
 
-			svbool_t pg3 = svwhilelt_b64(j, incl_count);
-
 			Dgrow = &Dg_row[j][dgi];
 			pdbr = dbr[j];
 			Dgrow1 = &Dg_row[j + 1][dgi];
 			pdbr1 = dbr[j + 1];
 
-			tmp1 = svmla_f64_x(pg3, svmla_f64_x(pg3, tmp1, pdbr, Dgrow[0]), pdbr1, Dgrow1[0]);
+			tmp1 = svmla_f64_x(svptrue_b64(), svmla_f64_x(svptrue_b64(), tmp1, pdbr, Dgrow[0]), pdbr1, Dgrow1[0]);
 		}
 		dgi++;
-		tmp1 = svmul_f64_x(pg, tmp1, avx_Scale)
-		svst1_f64(pg, &dyda[i], tmp1);
+		tmp1 = svmul_f64_x(svptrue_b64(), tmp1, avx_Scale);
+		svst1_f64(svptrue_b64(), &dyda[i], tmp1);
 	}
+
 	/* Ders. of brightness w.r.t. rotation parameters */
-	avx_dyda1 = svadd_f64(svget2_f64(avx_dyda1, 0), svget2_f64(avx_dyda1, 1));
-	avx_dyda1 = svadd_f64(avx_dyda1, svext_f64(avx_dyda1, svres_f64(), 1));
-	avx_dyda1 = svmul_f64(avx_dyda1, avx_Scale);
-	svst1_f64(pg, g, avx_dyda1);
-	dyda[ncoef0 - 3 + 1 - 1] = g[0];
-	dyda[ncoef0 - 3 + 2 - 1] = g[1];
-	avx_dyda3 = svadd_f64(svget2_f64(avx_dyda3, 0), svget2_f64(avx_dyda3, 1));
-	avx_dyda3 = svadd_f64(avx_dyda3, svext_f64(avx_dyda3, svres_f64(), 1));
-	avx_dyda3 = svmul_f64(avx_dyda3, avx_Scale);
-	svst1_f64(pg, g, avx_dyda3);
-	dyda[ncoef0 - 3 + 3 - 1] = g[0];
+	dyda[ncoef0 - 3 + 1 - 1] = svaddv_f64(svptrue_b64(), avx_dyda1) * Scale;
+	dyda[ncoef0 - 3 + 2 - 1] = svaddv_f64(svptrue_b64(), avx_dyda2) * Scale;
+	dyda[ncoef0 - 3 + 3 - 1] = svaddv_f64(svptrue_b64(), avx_dyda3) * Scale;
 	/* Ders. of br. w.r.t. cl, cls */
-	avx_d = svadd_f64(svget2_f64(avx_d, 0), svget2_f64(avx_d1, 1));
-	avx_d = svadd_f64(avx_d, svext_f64(avx_d, svres_f64(), 1));
-	//avx_d = _mm256_add_pd(avx_d, _mm256_permute2f128_pd(avx_d, avx_d, 1));
-	avx_d = svmul_f64(avx_d, avx_Scale);
-	avx_d = svmul_f64(avx_d, avx_cl1);
-	svst1_f64(pg, g, avx_d);
-	dyda[ncoef - 1 - 1] = g[0];
-	dyda[ncoef - 1] = g[1];
+	dyda[ncoef - 1 - 1] = svaddv_f64(svptrue_b64(), avx_d) * Scale * cl;
+	dyda[ncoef - 1] = svaddv_f64(svptrue_b64(), avx_d1) * Scale;
 
 	/* Ders. of br. w.r.t. phase function params. */
 	for (i = 1; i <= Nphpar; i++)
 		dyda[ncoef0 + i - 1] = br * dphp[i];
-	/*     dyda[ncoef0+1-1] = br * dphp[1];
-		 dyda[ncoef0+2-1] = br * dphp[2];
-		 dyda[ncoef0+3-1] = br * dphp[3];*/
 
-		 /* Scaled brightness */
+	/* Scaled brightness */
 	br *= Scale;
-
-	/*printf("\ndyda[208]:\n");
-	printf("_dyda[] ={");
-	for(int q = 0; q <=207; q++)
-	{
-		printf("%.30f, ", dyda[q]);
-	}
-	printf("};\n"); */
-
-	//return(br);
 }
